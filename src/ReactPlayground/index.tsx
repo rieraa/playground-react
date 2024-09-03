bf01277c0a8bbe2a365c19a9ccf2691be72de65a
linwenyi <wenyi.linwy@dtzhejiang.com>
2024-09-03 10:55:14 +0800

    🐛 移除页面滚动条 修复文件列表高度收缩

--- src/ReactPlayground/index.tsx
+++ src/ReactPlayground/index.tsx
@@ -3,10 +3,11 @@ import 'allotment/dist/style.css'
 import Header from './components/Header'
 import CodeEditor from './components/CodeEditor'
 import Preview from './components/Preview'
+import style from './index.module.scss'
 
 export default function ReactPlayground() {
   return (
-    <div style={{ height: '100vh' }}>
+    <div className={style.container}>
       <Header />
       <Allotment defaultSizes={[100, 100]}>
         <Allotment.Pane minSize={0}>
