470ad675a9e835ddd134f33e30678fceb5d32926
linwenyi <wenyi.linwy@dtzhejiang.com>
2024-09-03 17:50:34 +0800

    🤷 保存编译结果todo

--- src/ReactPlayground/components/Preview/compile.ts
+++ src/ReactPlayground/components/Preview/compile.ts
@@ -1,92 +1,118 @@
-import { transform } from '@babel/standalone'
-import { Files, File } from '../../PlaygroundContext'
-import { ENTRY_FILE_NAME } from '../../files'
-import { PluginObj } from '@babel/core'
+import { transform } from "@babel/standalone";
+import { File, Files } from "../../PlaygroundContext";
+import { ENTRY_FILE_NAME } from "../../files";
+import { PluginObj } from "@babel/core";
 
 export const beforeTransformCode = (filename: string, code: string) => {
-  let _code = code
+  let _code = code;
   const regexReact = /^import\s+React\s+from\s+['"]react['"];?$/m;
-  if ((filename.endsWith('.jsx') || filename.endsWith('.tsx')) && !regexReact.test(code)) {
-    _code = `import React from 'react';\n${code}`
+  if (
+    (filename.endsWith(".jsx") || filename.endsWith(".tsx")) &&
+    !regexReact.test(code)
+  ) {
+    _code = `import React from 'react';\n${code}`;
   }
-  return _code
-}
+  return _code;
+};
 
 export const babelTransform = (
   filename: string,
   code: string,
-  files: Files
+  files: Files,
 ) => {
   const _code = beforeTransformCode(filename, code);
-  // main.tsx => 
-  let result = ''
+  // main.tsx =>
+  let result = "";
   try {
-    result = transform(_code, {
-      presets: ['react', 'typescript'],
+    // todo 深度优先遍历 二叉树也回顾一下
+    result = transform(code, {
+      presets: ["react", "typescript"],
       filename,
-      plugins: [customResolver(files)],
+      plugins: [customResolver(files, filename)],
       retainLines: true,
-    }).code!
+    }).code!;
   } catch (e) {
-    console.error('编译出错', e)
+    console.error("编译出错", e);
   }
 
-  return result
-}
+  // 1.main
+
+  return result;
+};
 
 /**
  * 自定义插件自定义导入文件来源
  * @param files
  */
-function customResolver(files: Files): PluginObj {
+function customResolver(files: Files, filename: string): PluginObj {
   return {
     visitor: {
       ImportDeclaration(path) {
-        const modulePath = path.node.source.value
-        if (modulePath.startsWith('.')) {
-          const moduleFile = getModuleFile(files, modulePath)
+        debugger;
+        const modulePath = path.node.source.value;
 
-          if (!moduleFile) return
-          if (moduleFile.name.endsWith('.css')) {
-            path.node.source.value = css2Js(moduleFile)
-          } else if (moduleFile.name.endsWith('.json')) {
-            path.node.source.value = json2Js(moduleFile)
+        if (modulePath.startsWith(".")) {
+          const moduleFile = getModuleFile(files, modulePath);
+
+          if (!moduleFile) return;
+          if (moduleFile.name.endsWith(".css")) {
+            path.node.source.value = css2Js(moduleFile);
+          } else if (moduleFile.name.endsWith(".json")) {
+            path.node.source.value = json2Js(moduleFile);
           } else {
             // tsx文件存在嵌套引用
             path.node.source.value = URL.createObjectURL(
-              new Blob([
-                babelTransform(moduleFile.name, moduleFile.value, files),
-              ], {
-                type: 'application/javascript',
-              })
-            )
+              new Blob(
+                [babelTransform(moduleFile.name, moduleFile.value, files)],
+                {
+                  type: "application/javascript",
+                },
+              ),
+            );
+            // files[moduleFile.name].compiledCode = babelTransform(
+            //   moduleFile.name,
+            //   moduleFile.value,
+            //   files,
+            // );
+            console.log(
+              "🥘 ~ file:compile.ts method:ImportDeclaration line:76 param:filename -----",
+              filename,
+            );
+            console.log(
+              "🥘 ~ file:compile.ts method:ImportDeclaration line:72 param:moduleFile.name -----",
+              moduleFile.name,
+            );
+            // console.log(
+            //   "🥘 ~ file:compile.ts method:ImportDeclaration line:73 param:files[moduleFile.name].compiledCode -----",
+            //   files[moduleFile.name].compiledCode,
+            // );
           }
         }
       },
     },
-  }
+  };
 }
 const getModuleFile = (files: Files, modulePath: string) => {
-  let moduleName = modulePath.split('./').pop() || ''
-  if (!moduleName.includes('.')) {
+  let moduleName = modulePath.split("./").pop() || "";
+  if (!moduleName.includes(".")) {
     moduleName =
       Object.keys(files).find((key) => {
-        const baseName = key.split('.').shift()
+        const baseName = key.split(".").shift();
         return (
           baseName === moduleName &&
-          (key.endsWith('.ts') ||
-            key.endsWith('.tsx') ||
-            key.endsWith('.js') ||
-            key.endsWith('.jsx'))
-        )
-      }) || moduleName
+          (key.endsWith(".ts") ||
+            key.endsWith(".tsx") ||
+            key.endsWith(".js") ||
+            key.endsWith(".jsx"))
+        );
+      }) || moduleName;
   }
 
-  return files[moduleName]
-}
+  return files[moduleName];
+};
 
 const css2Js = (file: File) => {
-  const randomId = new Date().getTime()
+  const randomId = new Date().getTime();
   const js = `
 (() => {
     const stylesheet = document.createElement('style')
@@ -97,18 +123,22 @@ const css2Js = (file: File) => {
     const styles = document.createTextNode(\`${file.value}\`)
     stylesheet.appendChild(styles)
 })()
-    `
-  return URL.createObjectURL(new Blob([js], { type: 'application/javascript' }))
-}
+    `;
+  return URL.createObjectURL(
+    new Blob([js], { type: "application/javascript" }),
+  );
+};
 
 const json2Js = (file: File) => {
-  const js = `export default ${file.value}`
-  return URL.createObjectURL(new Blob([js], { type: 'application/javascript' }))
-}
+  const js = `export default ${file.value}`;
+  return URL.createObjectURL(
+    new Blob([js], { type: "application/javascript" }),
+  );
+};
 
 export const compile = (files: Files) => {
   // 2.获取入口文件
-  const main = files[ENTRY_FILE_NAME]
+  const main = files[ENTRY_FILE_NAME];
   // 3.入口代码转译
-  return babelTransform(ENTRY_FILE_NAME, main.value, files)
-}
+  return babelTransform(ENTRY_FILE_NAME, main.value, files);
+};
